76c7980d9bcfb51abb485a554408ea9be4acb5c4
Roman <rbaidachenko@gmail.com>
2023-12-29 21:47:57 +0000

    setting the practice project

---
+++ src/test/java/roman/automation_exercise/runners/FailedTestRunner.java
@@ -0,0 +1,4 @@
+package roman.automation_exercise.runners;
+
+public class FailedTestRunner {
+}
---
+++ src/test/java/roman/automation_exercise/utils/Driver.java
@@ -0,0 +1,45 @@
+package roman.automation_exercise.utils;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class Driver {
+    private static InheritableThreadLocal<WebDriver> driverPool = new InheritableThreadLocal<>();
+    private Driver(){};
+
+    public static WebDriver getDriver() {
+        if (driverPool.get() == null) {
+            String browserType = ConfigReader.getProperty("browser");
+            if (browserType.equalsIgnoreCase("chrome"))
+                driverPool.set(new ChromeDriver());
+            else if (browserType.equalsIgnoreCase("firefox"))
+                driverPool.set(new FirefoxDriver());
+            else if (browserType.equalsIgnoreCase("edge"))
+                driverPool.set(new EdgeDriver());
+            else if (browserType.equalsIgnoreCase("headless-chrome")) {
+                var options = new ChromeOptions();
+                options.addArguments("--headless=new");
+                driverPool.set(new ChromeDriver(options));
+            }
+            else
+                throw new IllegalArgumentException();
+
+            driverPool.get().manage().window().maximize();
+            driverPool.get().manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        }
+
+        return driverPool.get();
+    }
+
+    public static void closeDriver() {
+        if (driverPool.get() != null) {
+            driverPool.get().quit();
+            driverPool.remove();
+        }
+    }
+}
